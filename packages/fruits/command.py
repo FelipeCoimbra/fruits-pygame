892bb24983a4a96c9f80a181639048fbecd050c6
Unknown <felipecoimbra97@gmail.com>
2018-03-23 19:53:57 -0300

    Finished InputHandler

---
+++ packages/fruits/command.py
@@ -0,0 +1,25 @@
+
+
+class Command:
+
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+    SPACE = 4
+    ESCAPE = 5
+
+    def __init__(self, action):
+        self.count = 0
+        self.action = action
+
+    def increment_count(self):
+        self.count = self.count + 1
+
+    def get_action(self):
+        return self.action
+
+    def get_count(self):
+        return self.count
+
+
--- packages/fruits/game.py
+++ packages/fruits/game.py
@@ -1,9 +1,12 @@
 import pygame
-
+import fruits.input
+import fruits.control_room
 
 class FruitsGame(object):
     def __init__(self, main_window) -> None:
         self.main_window = main_window
+        self.__input_handler = fruits.input.InputHandler()
+        self.__control_room = fruits.control_room.ControlRoom()
 
     def loop(self) -> None:
         clock = pygame.time.Clock()
@@ -11,9 +14,28 @@ class FruitsGame(object):
         while True:
             delta = clock.tick(60)
 
-            for event in pygame.event.get():
-                if event == pygame.QUIT:
+            quit_events = pygame.event.get(pygame.QUIT)
+            if len(quit_events) > 0:
                     return
+
+            self.__input_handler.update_user_command(pygame.event.get())
+
+            self.__control_room.manage(self.__input_handler.get_user_commands())
+
+            self.__drawer.control
+
+            '''
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        print("Space bar pressed down.")
+                    elif event.key == pygame.K_ESCAPE:
+                        print("Escape key pressed down.")
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_SPACE:
+                        print("Space bar released.")
+                    elif event.key == pygame.K_ESCAPE:
+                        print("Escape key released.")'''
 
             pygame.display.update()
 
